7fdcdb9bd8ce7bc66d633c8cf0ea42710b586027
Adria Marcet <adriamarcetrovira@gmail.com>
2015-10-12 20:09:46 -0400

    Conditional title for categories, shows description or not

--- taxonomy-musica_cat.php
+++ taxonomy-musica_cat.php
@@ -5,7 +5,16 @@
 ); ?>
 <section>
 	<?php if (have_posts()) : ?>
-	<h1 class="title__category" ><?php single_cat_title( $prefix = 'Todos los ', $display = true ); ?></h1>
+	<h1 class="title__category" >
+		<?php 
+			$termDesc = term_description($term_id, $taxonomy); 
+			if( $termDesc) { 
+				echo wp_strip_all_tags( $termDesc );
+			}else { 
+				single_cat_title( $prefix = 'Todos los ', $display = true ); 
+			};
+		?>
+	</h1>
 	<?php endif; ?>
 	<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/list__music' ) ); ?>
 	<a href="<?php echo get_post_type_archive_link( 'musica' ); ?>" class="btn btn__primary">Más música</a>
--- taxonomy-recetas_cat.php
+++ taxonomy-recetas_cat.php
@@ -5,7 +5,16 @@
 ); ?>
 <?php if (have_posts()) : ?>	
 <section>
-	<h1 class="title__category"><?php single_cat_title( $prefix = 'Todas las recetas de la categoría ', $display = true ); ?></h1>
+	<h1 class="title__category">
+		<?php 
+			$termDesc = term_description($term_id, $taxonomy); 
+			if( $termDesc) { 
+				echo wp_strip_all_tags( $termDesc );
+			}else { 
+				single_cat_title( $prefix = 'Todas las recetas de la categoría ', $display = true ); 
+			};
+		?>
+	</h1>
 	<?php endif; ?>
 	<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/list__food' ) ); ?>
 	<a href="<?php echo get_post_type_archive_link( 'recetas' ); ?>" class="btn btn__primary">Más recetas</a>
